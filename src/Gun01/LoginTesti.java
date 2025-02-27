6f2b2d098e64d4ed0b89a0c73cc50d0dc3ee49b7
ibcan9 <canibr97@gmail.com>
2025-02-27 20:33:58 +0300

    yeni login testi yapıldı.

--- src/Gun01/LoginTesti.java
+++ src/Gun01/LoginTesti.java
@@ -6,10 +6,9 @@ public class LoginTesti {
         // Bu bölüm Login testi için, yeni şifre modeline göre yazıldı.
         System.out.println("Yeni Login Testi");
 
-        // çok satır yazdım ve kodları bitirdim.
-        System.out.println("Login Testi 1");
-
         System.out.println("Sepet testi yazıldı bitirildi, 3 günlük kod yazıldı");
+
+        System.out.println("Yeni login testi yapıldı, 7 gün uğraştım");
 
 
     }
