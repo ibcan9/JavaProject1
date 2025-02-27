22ecf8e07a923693eac34998a3b0d00bc2d2bce5
ibcan9 <canibr97@gmail.com>
2025-02-27 20:22:19 +0300

    Login tesi asama 1 (Jiradaki gelen taskdaki başlık

---
+++ src/Gun01/LoginTesti.java
@@ -0,0 +1,11 @@
+package Gun01;
+
+public class LoginTesti {
+    public static void main(String[] args) {
+
+        // Bu bölüm Login testi için, yeni şifre modeline göre yazıldı.
+        System.out.println("Yeni Login Testi");
+
+
+    }
+}
